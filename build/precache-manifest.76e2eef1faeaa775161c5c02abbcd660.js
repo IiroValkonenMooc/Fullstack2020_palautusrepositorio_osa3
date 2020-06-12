145b2781ada88b8a9a817bb7d004cab1ae8543e8
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-06-12 21:42:23 +0300

    tehvävä 3.20 build

---
+++ build/precache-manifest.76e2eef1faeaa775161c5c02abbcd660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8ba4719594a2e70d00083a5c476994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d07b181c819668d8f01",
+    "url": "/static/css/main.80696b58.chunk.css"
+  },
+  {
+    "revision": "9f2e940fbae04d20d51d",
+    "url": "/static/js/2.bd33f1b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd33f1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d07b181c819668d8f01",
+    "url": "/static/js/main.3a4f5578.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
